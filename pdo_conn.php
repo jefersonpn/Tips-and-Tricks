325ed0448e2f57cf2047e6ef3605e75d8ec1d6f4
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2023-03-13 20:56:48 +0100

    Update pdo_conn.php

--- pdo_conn.php
+++ pdo_conn.php
@@ -13,3 +13,20 @@ try
         echo 'Error message: <strong>'.$ex->getMessage().'</strong>';
      }
 
+// insert and delete 
+function insert_product($name, $brand, $model, $quantity)
+{
+    include 'connection1.php';
+
+    //inserire questo codice dal php- INSERT query with positional placeholders
+    $sql = "INSERT INTO prodotto (nome, marchio, modello, quantita) VALUES (?,?,?,?)";
+    $pdo->prepare($sql)->execute([$name, $brand, $model, $quantity]);
+
+}
+
+function delete_product($id){
+    include 'connection1.php';
+
+    $sql = "DELETE FROM prodotto WHERE id=?";
+    $pdo->prepare($sql)->execute([$id]);
+}
